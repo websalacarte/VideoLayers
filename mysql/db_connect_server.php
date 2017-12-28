ad7e43504e7b5b9821ad31295af38693eb0f48be
Josep Portell Guarch <josep@websalacarte.com>
2017-12-28 16:54:41 +0100

    Add files via upload

---
+++ mysql/db_connect_server.php
@@ -0,0 +1,23 @@
+<?php 
+
+	//$connect = mysql_connect('server', 'user', 'password');
+	//$connect = mysql_connect('websalac_videolayers', 'vladmin', '8464vdEmm') or die("Problems!");
+
+
+
+//$connect = mysql_connect('localhost', 'websalac_vladmin', '8464vdEmm') or die("Problems!" . mysql_error());
+$connect = mysql_connect('localhost', 'websalac_vladmin', '8464vdEmm') or die("Problems!" . mysql_error());
+
+	//$connect = mysql_connect('localhost', 'root', '') or die("Problems!" . mysql_error());
+
+//$db = mysql_select_db('websalac_videolayers');
+
+//if (!isset ($db)) {
+	$db = mysql_select_db('websalac_videolayersv09');
+	//$db = mysql_select_db('websalac_voctours');					// local portatil
+//}
+
+if( !$connect ) {
+}
+
+?>
