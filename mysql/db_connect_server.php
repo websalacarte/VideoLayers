47907707bdf3fddd57aeaa738a50f21df8e24fab
Josep Portell Guarch <josep@websalacarte.com>
2017-12-28 17:21:06 +0100

    Update db_connect_server.php

--- mysql/db_connect_server.php
+++ mysql/db_connect_server.php
@@ -1,16 +1,5 @@
 <?php 
-
-	//$connect = mysql_connect('server', 'user', 'password');
-	//$connect = mysql_connect('websalac_videolayers', 'vladmin', '8464vdEmm') or die("Problems!");
-
-
-
-//$connect = mysql_connect('localhost', 'websalac_vladmin', '8464vdEmm') or die("Problems!" . mysql_error());
-$connect = mysql_connect('localhost', 'websalac_vladmin', '8464vdEmm') or die("Problems!" . mysql_error());
-
-	//$connect = mysql_connect('localhost', 'root', '') or die("Problems!" . mysql_error());
-
-//$db = mysql_select_db('websalac_videolayers');
+$connect = mysql_connect('{yourDBservername}', '{yourDBusername}', '{yourDBpssword}') or die("Problems!" . mysql_error());
 
 //if (!isset ($db)) {
 	$db = mysql_select_db('websalac_videolayersv09');
